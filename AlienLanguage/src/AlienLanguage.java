fc58ac2a1cb7d514ff2e6d26369a9bf8b0386750
Yaoming <yao4ming@gmail.com>
2016-03-07 22:37:49 -0500

    updated

--- AlienLanguage/src/AlienLanguage.java
+++ AlienLanguage/src/AlienLanguage.java
@@ -1,5 +1,45 @@
-/**
- * Created by yaoming on 3/7/16.
- */
-public class AlienLanguage {
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class AlienLanguage{
+
+   static String[] words;
+
+    public static void main(String []args) {
+
+        File file = new File("input.in");
+        try (Scanner in = new Scanner(file); PrintWriter writer = new PrintWriter("output.txt", "utf-8")) {
+
+            int L = in.nextInt();
+            int D = in.nextInt();
+            int N = in.nextInt();
+            in.nextLine();
+            words = new String[D];
+
+            //store known words
+            for (int i = 0; i < D; i++) {
+                words[i] = in.nextLine();
+            }
+
+            //search known words for regex pattern
+            for (int i = 0, testCase = 1; i < N; i++, testCase++) {
+
+                int found = 0;
+                String pattern = in.nextLine().replaceAll(Pattern.quote("("), "[").replaceAll(Pattern.quote(")"), "]");
+
+                for (int j = 0; j < D; j++) {
+                    if (words[j].matches(pattern)) {
+                        found++;
+                    }
+                }
+                writer.println("Case #" + testCase + ": " + found);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
